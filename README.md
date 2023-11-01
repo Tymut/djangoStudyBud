7893b1b0014c714c63cffd08936b264c1a6572ac
Tymut <tymutowski@protonmail.com>
2023-11-01 20:14:07 +0100

    Changing MEDIA_ROOT path / Updating URL's in templates

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 ### It's my first Django Project which is based on Dennis Ivy Django tutorial. This helps me to understand how Django works and how to create Backend oriented web applications
 
 ### Don't take this as my own serious project ;D
-
--- studybud/settings.py
+++ studybud/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/images/userImages/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'static/images/userImages/'
 #STATIC_ROOT =
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
